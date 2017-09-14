6ee2de07d173aeccf92d2fa498a68c68adcf3ebc
kojitaniguchi <tanigu0511@gmail.com>
2017-09-15 01:10:00 +0900

    slack-todo

---
+++ slack-todo/scripts/todo.js
@@ -0,0 +1,37 @@
+// Description:
+//   TODO を管理することができるボットです
+// Commands:
+//   koji-hubot-study todo     - TODO を作成
+//   koji-hubot-study done     - TODO を完了にする
+//   koji-hubot-study del      - TODO を消す
+//   koji-hubot-study list     - TODO の一覧表示
+//   koji-hubot-study donelist - 完了した TODO の一覧表示
+
+'use strict';
+const todo = require('todo');
+
+
+module.exports = (robot) => {
+    robot.respond(/todo (.+)/i, (msg) => {
+        const task = msg.match[1].trim();
+        todo.todo(task);
+        msg.send('追加しました: ' + task);
+    });
+     robot.respond(/done (.+)/i, (msg) => {
+        const task = msg.match[1].trim();
+        todo.done(task);
+        msg.send('完了にしました: ' + task);
+    });
+    robot.respond(/del (.+)/i, (msg) => {
+        const task = msg.match[1].trim();
+        todo.del(task);
+        msg.send('削除しました: ' + task);
+    });
+    robot.respond(/list/i, (msg) => {
+        msg.send(todo.list().join('\n'));
+    });
+    robot.respond(/donelist/i, (msg) => {
+        msg.send(todo.donelist().join('\n'));
+    });
+};
+
